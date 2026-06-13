56e40dc6dab5dff90f291743107156735d0701ac
Noah Reardon <nmreardon05@gmail.com>
2026-06-13 00:04:48 -0400

    added new simulink file for controlling the pendulum with pid

--- matlab_simulation/inverted_pendulum_testing.m
+++ matlab_simulation/inverted_pendulum_testing.m
@@ -2,37 +2,39 @@ load_parameters();
 
 out = sim("pendulum_model.slx");
 
-figure("Name", "Inverted pendulum response to 10V step input");
-
-subplot(5, 1, 1)
+subplot(6, 1, 1)
 plot(out.tout, out.torque1.Data)
 ylim([0, 0.2])
 xlabel("Time (s)")
 ylabel("(Nm)")
 title("Applied torque")
 
-subplot(5, 1, 2)
+subplot(6, 1, 2)
 plot(out.tout, out.arm_angle.Data)
 xlabel("Time (s)")
 ylabel("(rad)")
 title("Arm angle")
 
-subplot(5, 1, 3)
+subplot(6, 1, 3)
 plot(out.tout, out.arm_velocity.Data)
 xlabel("Time (s)")
 ylabel("(rad/s)")
 title("Arm velocity")
 
-subplot(5, 1, 4)
+subplot(6, 1, 4)
 plot(out.tout, out.pendulum_angle.Data)
 xlabel("Time (s)")
 ylabel("(rad)")
 title("Pendulum angle")
 
-subplot(5, 1, 5)
+subplot(6, 1, 5)
 plot(out.tout, out.pendulum_velocity.Data)
 xlabel("Time (s)")
 ylabel("(rad/s)")
 title("Pendulum velocity")
 
-
+subplot(6, 1, 6)
+plot(out.tout, out.input_signal.data)
+title("Input signal")
+xlabel("Time (s)")
+ylabel("Volts (V)")
