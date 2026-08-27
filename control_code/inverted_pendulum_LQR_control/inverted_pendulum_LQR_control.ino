604064d27d1ef1b2d45e871c2c3a7ba1ce64df46
Noah <nmreardon05@gmail.com>
2026-08-27 16:30:27 -0400

    actually converted to estimated current mode now

--- control_code/inverted_pendulum_LQR_control/inverted_pendulum_LQR_control.ino
+++ control_code/inverted_pendulum_LQR_control/inverted_pendulum_LQR_control.ino
@@ -3,21 +3,17 @@
 // DEFINE CONSTANTS
 double zero_offset = 0;
 // double k[4] = {-0.1, 7.7067, -0.1213, 0.7476};
-// double k[4] = {-0.0071, 0.5524, -0.0086, 0.0530}; // pretty good performance
-// double k[4] = {-0.04, 0.5524, -0.0086, 0.0530}; // test for controlling motor angle better
-// double k[4] = {-0.0030, 0.4900, -0.0035, 0.0465};
-// double k[4] = {-0.0033, 0.5366, -0.0039, 0.0510};
-double k[4] = {-0.0045, 0.4900, -0.0058, 0.0621};
+double k[4] = {-0.0071, 0.5524, -0.0086, 0.0530};
 
 
-//motor parameters
+    //motor parameters
 const double motor_Kv = 20;
 const double motor_Kt = 60/(TWO_PI * motor_Kv);
 const double motor_Ke = motor_Kt;
 const double motor_R = 13.7;
 
+static double filt_v_cmd = 0.0;
 static double filt_pend_vel = 0.0;
-static double filt_motor_vel = 0.0;
 
 // initialize motor and driver
 BLDCMotor motor = BLDCMotor(11, motor_R, motor_Kv);
@@ -39,9 +35,7 @@ void doB() {
 }
 
 double torque_to_voltage(double torque_cmd, double motor_vel) {
-  // return (motor_R / motor_Kt) * torque_cmd + motor_Ke * motor_vel;
-  return (motor_R / motor_Kt) * torque_cmd;
-  // return torque_cmd * 30;
+  return (motor_R / motor_Kt) * torque_cmd + motor_Ke * motor_vel;
 
 }
 
@@ -74,22 +68,20 @@ void setup() {
   motor.init();
   motor.initFOC();
 
+  // set motor current limit
+  motor.current_limit = 1.0;
   // set motor voltage limit
-  motor.voltage_limit = 8;
+  motor.voltage_limit = 8.0;
 
   // set torque control mode
   motor.torque_controller = TorqueControlType::estimated_current;
-  // set motion control mode
-  motor.controller = MotionControlType::torque;
 
-
+  Serial.println("Motor ready");
   delay(1000);
 }
 
 unsigned long lastTime = 0;
-const unsigned long sampleTime = 1000;
-double last_pend_angle = 0.0;
-double last_motor_angle = 0.0;
+const unsigned long sampleTime = 200;
 
 void loop() {
   // loop the FOC algorithm as fast as possible
@@ -98,108 +90,40 @@ void loop() {
   unsigned long currentTime = micros();
 
   if (currentTime - lastTime >= sampleTime) {
-    double dt = (currentTime - lastTime) / 1000000.0;
-
     lastTime = currentTime;
 
     // get the new sensor reading
     sensor.update();
     double current_angle = (sensor.getAngle() - zero_offset);
-    // current_angle = fmod(current_angle + TWO_PI, TWO_PI);
+    current_angle = fmod(current_angle + TWO_PI, TWO_PI);
 
     // get the new motor position
     double motor_angle = encoder.getAngle();
 
     // get motor velocity
     double motor_velocity = motor.shaft_velocity;
-    // double motor_velocity = (motor_angle - last_motor_angle) /  dt;
-    // last_motor_angle = motor_angle;
 
     // get pendulum velocity
-    // double pendulum_velocity = sensor.getVelocity();
+    double pendulum_velocity = sensor.getVelocity();
 
-    double pendulum_velocity = (current_angle - last_pend_angle) / dt;
-    last_pend_angle = current_angle;
-
-
-    filt_pend_vel = 0.7 * filt_pend_vel + 0.3 * pendulum_velocity;
-    // filt_motor_vel = 0.7 * filt_motor_vel + 0.3 * motor_velocity;
-
-    double voltage_command = 0.0;
-    double torque_command = 0.0;
-  
+    filt_pend_vel = 0.9 * filt_pend_vel + 0.1 * pendulum_velocity;
 
 
     if (current_angle > 2.967 && current_angle < 3.316) {
       // if the pendulum is within the bounds, update the torque and command the motor
-      torque_command = -1 * ((k[0] * motor_angle) + (k[1] * (current_angle - PI)) + (k[2] * motor_velocity) + (k[3] * filt_pend_vel));
-      voltage_command = torque_to_voltage(torque_command, motor_velocity);
+      double torque_command = -1 * ((k[0] * motor_angle) + (k[1] * (current_angle - PI)) + (k[2] * motor_velocity) + (k[3] * pendulum_velocity));
+      motor.target = torque_command / motor_Kt;
+      // Serial.println(torque_command / motor_Kt);
+      motor.move();
 
-      // voltage_command = torque_command * 20;
-
-
-      voltage_command = constrain(
-        voltage_command,
-        -motor.voltage_limit,
-        motor.voltage_limit);
-
-      // filt_v_cmd = 0.9 * filt_v_cmd + 0.1 * voltage_command;
-
-
-      // uncomment to display states
-      // double u1 = -k[0] * motor_angle;
-      // double u2 = -k[1] * (current_angle - PI);
-      // double u3 = -k[2] * motor_velocity;
-      // double u4 = -k[3] * filt_pend_vel;
-      // Serial.print(u1); Serial.print(",");
-      // Serial.print(u2); Serial.print(",");
-      // Serial.print(u3); Serial.print(",");
-      // Serial.print(u4); Serial.print(",");
-
-
-      // uncomment to display variables on the serial monitor with bounds
-      // Serial.print(-10);
-      // Serial.print(",");
-      // Serial.print(10);
-      // Serial.print(",");
-      // Serial.println(voltage_command / 3);
-      // Serial.print(",");
-      // filt_v_cmd = constrain(filt_v_cmd, -motor.voltage_limit, motor.voltage_limit);
-      // Serial.println(voltage_command);
-  
-      // motor.move(voltage_command);
-      // voltage_command = 2*sin(2*PI*currentTime*0.000001);
-      // motor.move(voltage_command);
-
-
-      double error = PI - current_angle;
-      // Serial.println(error * 50);
-      motor.move(error * 50);
-
-      
-      // delay(300);
     } else {
 
       // set motor torque to zero if it falls outside the range
-      motor.move(0);
-      // voltage_command = 2*sin(2*PI*currentTime*0.000001);
-      // motor.move(voltage_command);
+      motor.target = 0;
+      motor.move();
     }
     
-    Serial.print(currentTime);
-    Serial.print(",");
-    Serial.print(motor_angle);
-    Serial.print(",");
-    Serial.print(current_angle - PI);
-    Serial.print(",");
-    Serial.print(motor_velocity);
-    Serial.print(",");
-    Serial.print(filt_pend_vel);
-    Serial.print(",");
-    Serial.print(voltage_command);
-    Serial.print(",");
-    Serial.println(torque_command);
-
+  
 
   }
 }
